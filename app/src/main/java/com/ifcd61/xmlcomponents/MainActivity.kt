24c9ab71b8ae9f1ed458afff0548da1559666575
Patricio <pechagueb@gmail.com>
2025-09-25 11:07:13 +0100

    Initial commit

---
+++ app/src/main/java/com/ifcd61/xmlcomponents/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.ifcd61.xmlcomponents
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.card.MaterialCardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<MaterialButton>(R.id.btnBuy).setOnClickListener {
+            Toast.makeText(this,"Historial", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<MaterialButton>(R.id.btnSkip).setOnClickListener {
+            findViewById<MaterialCardView>(R.id.cvAd).visibility = View.GONE
+        }
+    }
+}
